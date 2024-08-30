17a95ba197ec189a24ec67f547515cbdfb267269
Tri Pham <phamthanhtri0712@gmail.com>
2024-08-30 08:44:29 +0700

    Page Introduce

--- src/pages/Introduce/index.js
+++ src/pages/Introduce/index.js
@@ -1,9 +1,61 @@
+import { FacebookOutlined, InstagramOutlined, RedditOutlined, TwitterOutlined } from "@ant-design/icons";
+import "./Introduce.scss";
+
 function Introduce() {
   return (
     <>
-      Page Introduce
+      <div className="introduce">
+        <h1 className="introduce__title">Giới thiệu chung</h1>
+        <p className="introduce__date">11:27 - 20/02/2024</p>
+        <p className="introduce__desc--item-1">
+          Được thành lập với mong muốn mang đến những căn nhà hiện đại, tinh tế và giàu chất kiến trúc nhất cho quý
+          khách hàng, UOM đã và đang mang đến cho bạn những mẫu thiết kế và thi công mới nhất phù hợp với xu hướng kiến
+          trúc hiện đại và không gian sử dụng thực tế nhất.
+        </p>
+        <h1 className="introduce__title">GIỚI THIỆU VỀ UOM</h1>
+        <p className="introduce__desc--item-2">
+          Được thành lập với mong muốn mang đến những căn nhà hiện đại, tinh tế và giàu chất kiến trúc nhất cho quý
+          khách hàng, UOM đã và đang mang đến cho bạn những mẫu thiết kế và thi công mới nhất phù hợp với xu hướng kiến
+          trúc hiện đại và không gian sử dụng thực tế nhất.
+        </p>
+        <p className="introduce__desc--item-3">
+          Hơn 100 dự án thiết kế kiến trúc nhà ở cũng như thiết kế biệt thự đã ra đời từ sự sáng tạo và năng động của
+          đội ngũ kiến trúc sư và kinh nghiệm của đội ngũ tư vấn-thi công để mang đến sự hài lòng cho quý khách hàng và
+          cho chính chúng tôi khi thẩm định sản phẩm của mình.
+        </p>
+        <p className="introduce__desc--item-4">
+          Yếu tố truyền thống của người Việt ta trong xây dựng nhà như phong thủy luôn được chú trọng để tạo ra các kiến
+          trúc hài hòa và ấn tượng nhất đến cho người ở. Mỗi căn nhà là cả một công trình kiến trúc với phối cảnh độc
+          đáo, cách thiết kế nội thất trí hợp lý và thực dụng tối đa đi cùng kỹ năng thi công đáng tin cậy . Rất nhiều
+          yếu tố đòi hỏi kỹ năng chuyên sâu và sự đầu tư nghiêm túc về thời gian, trí tuệ. Hãy để chúng tôi song hành
+          cùng quý vị trên con đường này.
+        </p>
+        <h1 className="introduce__title">ĐÔI LỜI VỀ NHÀ ĐẸP MỚI - UOM</h1>
+        <p className="introduce__desc--item-5">
+          Nhà đẹp mới là câu cửa miệng của tất cả khách hàng khi có nhu cầu xây dựng tổ ấm cho gia đình và đó là chuyện
+          lớn của đời người vì thế Nhà Đẹp Mới luôn tâm huyết mang đến những sản phẩm công trình có chất lượng và tính
+          thẩm mỹ cao, đội ngũ kiến trúc sư trẻ không ngừng lao động, sáng tạo để cho ra những ý tưởng kiến trúc mới,
+          độc đáo và phù hợp với mong muốn của khách hàng. Thế mạnh tạo nên thương hiệu Nhà Đẹp Mới chính là sự chuyên
+          môn hóa giữa cổ điển và hiện đại, hài hòa chúng với nhau để cho ra mỗi dự án một phong cách kiến trúc, không
+          gian sống riêng cho từng ngôi nhà.
+        </p>
+        <p className="introduce__desc--item-6">
+          Ngoài ra các bộ phận nội thất, bộ phận thiết kế kiến trúc và xây dựng luôn trao đổi kinh nghiệm và chia sẻ
+          những sáng kiến để xây dựng nên nhà đẹp mới năng động sáng tạo trong từng dự án mà chúng tôi tham gia. với
+          phương châm hợp tác tạo nhà đẹp, cùng nhau xây nhà đẹp và định hướng thành công phát triển. Nhà đẹp mới luôn
+          nỗ lực về sức lực và nhân lực để xây dựng uy tín thương hiệu và niềm tin với khách hàng đến từng dự án mà
+          chúng tôi cung cấp.
+        </p>
+        <p className="introduce__desc--social">
+          Chia sẻ:
+          <FacebookOutlined />
+          <InstagramOutlined />
+          <TwitterOutlined />
+          <RedditOutlined />
+        </p>
+      </div>
     </>
-  )
+  );
 }
 
 export default Introduce;
