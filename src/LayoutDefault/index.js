67d177d3ddafab0f23112728785666e2a1dfa79f
Tri Pham <phamthanhtri0712@gmail.com>
2024-09-06 15:58:38 +0700

    Page Home

--- src/LayoutDefault/index.js
+++ src/LayoutDefault/index.js
@@ -1,17 +1,35 @@
-import { Outlet } from "react-router-dom";
-import Footer from "~/components/Footer";
-import Header from "~/components/Header";
+/* eslint-disable react/jsx-no-target-blank */
+import { Outlet } from 'react-router-dom';
+import Footer from '~/components/Footer';
+import Header from '~/components/Header';
+import './LayoutDefault.scss';
 
 function LayoutDefault() {
   return (
     <>
       <Header />
-      <main>
+      <main className="main">
         <Outlet />
+
+        <div className="main__social">
+          <a href="https://zalo.me/0398694435" target="_blank">
+            <img className="main__social--icon" src={require('../assets/images/zalo_icon.png')} alt="zalo_icon" />
+          </a>
+          <a href="mailto:phamthanhtri@gmail.com" target="_blank">
+            <img className="main__social--icon" src={require('../assets/images/email_icon.png')} alt="email_icon" />
+          </a>
+          <a href="https://www.facebook.com/tri.phamthanh.1029/" target="_blank">
+            <img
+              className="main__social--icon"
+              src={require('../assets/images/messager_icon.png')}
+              alt="messager_icon"
+            />
+          </a>
+        </div>
       </main>
       <Footer />
     </>
-  )
+  );
 }
 
 export default LayoutDefault;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,24 @@
+import { NavLink } from 'react-router-dom';
+import './Home.scss';
+
 function Home() {
   return (
     <>
-      Page Home
+      <div className="home">
+        <div className="home__section-one">
+          <div className="container">
+            <p className="home__section-one__intro">XU HƯỚNG 2024</p>
+            <h1 className="home__section-one__title">THIẾT KẾ NỘI THẤT</h1>
+            <p className="home__section-one__desc">
+              Xu hướng thiết kế nội thất Bắc Âu được giới trẻ ưu ái sử dụng trong những năm gần nhờ vào trào lưu sống
+              xanh, bền vững và yêu môi trường với chi phí phù hợp
+            </p>
+            <NavLink to="/product" className="btn home__section-one__btn">MUA NGAY</NavLink>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Home;
